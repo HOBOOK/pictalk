036401195165cc38774c3cde41a495444601e516
박경호 <pkh879@gmail.com>
2020-07-07 01:19:23 +0900

    Add spring boot project for initialization

---
+++ src/main/java/com/endspring/pictalk/PictalkApplication.java
@@ -0,0 +1,13 @@
+package com.endspring.pictalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endspring/pictalk/PictalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endspring.pictalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
