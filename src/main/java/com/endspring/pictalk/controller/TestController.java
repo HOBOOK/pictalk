2d1467c5e74a30a07eec54daa716aaab9c236ca3
박경호 <pkh879@gmail.com>
2020-07-08 11:31:06 +0900

    Set main layout to configue single page

--- src/main/java/com/endspring/pictalk/controller/TestController.java
+++ src/main/java/com/endspring/pictalk/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String test(){
+        return "app/chat";
     }
 }
