d297523f990abf7ac35217f4dcfe3ee0dae90cb8
송치민 <52474694+songchimin@users.noreply.github.com>
2020-07-11 17:08:20 +0900

    Implement dashboard.html, account.html, account_Info.html, file.html and ManagementController for Management page

---
+++ src/main/java/com/endspring/pictalk/controller/ManagementController.java
@@ -0,0 +1,28 @@
+package com.endspring.pictalk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManagementController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "app/dashboard";
+    }
+
+    @GetMapping("/file")
+    public String file(){
+        return "app/file";
+    }
+
+    @GetMapping("/account")
+    public String account(){
+        return "app/account";
+    }
+
+    @GetMapping("/account_info")
+    public String accountInfo(){
+        return "app/account_Info";
+    }
+}
