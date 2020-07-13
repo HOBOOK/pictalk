46d840b9a93dff1b2312dbc1cef44d5f026bde0b
송치민 <vzcxds@gmail.com>
2020-07-13 20:45:04 +0900

    Add management.js(->account.html)

---
+++ src/main/resources/static/js/management.js
@@ -0,0 +1,126 @@
+'use strict';
+
+app.controller('accountCtrl', function ($scope) {
+    $scope.accounts = [{
+        no: "1",
+        profile: "profile.png",
+        email: "",
+        name: "박경호",
+        nickname: "호북이",
+        gender: "남",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 1,
+        state: 0
+    },{
+        no: "2",
+        profile: "profile.png",
+        email: "vzcxds@gamil.com",
+        name: "송치민",
+        nickname: "송치킨",
+        gender: "남",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 10,
+        state: 0
+    },{
+        no: "3",
+        profile: "profile.png",
+        email: "",
+        name: "최지은",
+        nickname: "최단비",
+        gender: "여",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 112,
+        state: 0
+    },{
+        no: "4",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    },{
+        no: "5",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    },{
+        no: "6",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    },{
+        no: "7",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    },{
+        no: "8",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    },{
+        no: "9",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    },{
+        no: "10",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    },{
+        no: "11",
+        profile: "profile.png",
+        email: "",
+        name: "",
+        nickname: "",
+        gender: "",
+        birthday: "",
+        joindate: "2020-07-09 20:00",
+        reportnum: 0,
+        state: 0
+    }];
+});
