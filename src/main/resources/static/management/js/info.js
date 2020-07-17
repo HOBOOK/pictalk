ee756dd28f1a946c704b7f83d65b80b31fd72539
송치민 <vzcxds@gmail.com>
2020-07-18 02:54:51 +0900

    Refactor manager page structure (management.css)

--- src/main/resources/static/management/js/info.js
+++ src/main/resources/static/management/js/info.js
@@ -1,15 +1,96 @@
-app.controller('infoController', function ($scope, $uibModalInstance, no, logs) {
-
-    console.log(logs);
+app.controller('infoController', function ($scope, $uibModalInstance, no) {
 
     $scope.close = function() {
         $uibModalInstance.dismiss('cancel');
     }
 
-    $scope.logReports = logs;
+    $scope.logs = [{
+        no: "1",
+        roomName: "채팅방",
+        content: "로그내역1",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "2",
+        roomName: "채팅방",
+        content: "로그내역2",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "3",
+        roomName: "채팅방",
+        content: "로그내역3",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "4",
+        roomName: "채팅방",
+        content: "로그내역4",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "5",
+        roomName: "채팅방",
+        content: "로그내역5",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "6",
+        roomName: "채팅방",
+        content: "로그내역6",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "7",
+        roomName: "채팅방",
+        content: "로그내역7",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "8",
+        roomName: "채팅방",
+        content: "로그내역8",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "9",
+        roomName: "채팅방",
+        content: "로그내역9",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "10",
+        roomName: "채팅방",
+        content: "로그내역10",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    },{
+        no: "11",
+        roomName: "채팅방",
+        content: "로그내역11",
+        logDate: "2020/07/13 24:00:00",
+        remark: ""
+    }];
+
+    $scope.reports = [{
+        no: "1",
+        roomName: "채팅방1",
+        content: "/management/img/jieun.jpg",
+        reportContent: "신고내용1",
+        userNo: "신고자번호",
+        reportDate: "2020/07/15 24:00:00",
+    },{
+        no: "2",
+        roomName: "채팅방2",
+        content: "/management/img/jieun.jpg",
+        reportContent: "신고내용2",
+        userNo: "신고자번호",
+        reportDate: "2020/07/15 24:00:00",
+    }];
+
 
     angular.element(document).ready( function () {
-        $("#log-tab").trigger("click");
+        $("#info-tab").trigger("click");
 
         dTable = $('#table-info');
         dTable.DataTable();
--- src/main/resources/static/management/js/management.js
+++ src/main/resources/static/management/js/management.js
@@ -4,193 +4,125 @@ app.controller("accountCtrl", function ($scope, $uibModal, $http, $document) {
     $scope.show="false";
     $scope.accounts = [{
         no: "1",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "박경호",
         nickname: "호북이",
         gender: "남",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 1,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 1,
         state: "on"
     },{
         no: "2",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "vzcxds@gamil.com",
         name: "송치민",
         nickname: "송치킨",
         gender: "남",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 10,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 10,
         state: "off"
     },{
         no: "3",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "최지은",
         nickname: "최단비",
         gender: "여",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 112,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 112,
         state: "ban"
     },{
         no: "4",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
     },{
         no: "5",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
     },{
         no: "6",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
     },{
         no: "7",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
     },{
         no: "8",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
     },{
         no: "9",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
     },{
         no: "10",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
     },{
         no: "11",
-        profile: "profile.png",
+        profile: "/management/img/profile.png",
         email: "",
         name: "",
         nickname: "",
         gender: "",
         birthday: "",
-        joindate: "2020-07-09 20:00",
-        reportnum: 0,
+        joinDate: "2020-07-09 20:00",
+        reportCount: 0,
         state: 0
-    }];
-
-    $scope.logReports = [{
-        no: "1",
-        roomName: "채팅방",
-        content: "로그내역1",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "2",
-        roomName: "채팅방",
-        content: "로그내역2",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "3",
-        roomName: "채팅방",
-        content: "로그내역3",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "4",
-        roomName: "채팅방",
-        content: "로그내역4",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "5",
-        roomName: "채팅방",
-        content: "로그내역5",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "6",
-        roomName: "채팅방",
-        content: "로그내역6",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "7",
-        roomName: "채팅방",
-        content: "로그내역7",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "8",
-        roomName: "채팅방",
-        content: "로그내역8",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "9",
-        roomName: "채팅방",
-        content: "로그내역9",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "10",
-        roomName: "채팅방",
-        content: "로그내역10",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
-    },{
-        no: "11",
-        roomName: "채팅방",
-        content: "로그내역11",
-        logDate: "2020/07/13 24:00:00",
-        remark: ""
     }];
 
 
@@ -204,9 +136,6 @@ app.controller("accountCtrl", function ($scope, $uibModal, $http, $document) {
                 no: function () {
                     return item.no;
                 },
-                logs: function () {
-                    return $scope.logReports;
-                }
             }
         });
         modalInstance.result.then(function (item) {
