060201a584652499c1e4f717835802099c385910
송치민 <vzcxds@gmail.com>
2020-07-20 11:58:07 +0900

    Add file.js(->file.html) Update file management list design

---
+++ src/main/resources/static/management/js/file.js
@@ -0,0 +1,45 @@
+app.controller("fileCtrl", function ($scope) {
+
+    $scope.rooms = [{
+        no: 0,
+        type: 0,
+        title: "방제목1AAAAaaaaaaaaaaaaaaaaaa"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목2방제목2방제목2방제목2방제목2방제목2"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목3"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목4"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목5"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목6"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목7"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목8"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목9"
+    },{
+        no: 0,
+        type: 0,
+        title: "방제목10"
+    }];
+
+});
