03041cd622026431fb8830532eab5c0e408ee32b
026854 <026854@naver.com>
2020-07-20 23:28:55 +0900

    Update mypage.html : change nav-background-color if selected

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,32 +8,42 @@ var tempUserModel = {
     nickname: "나다",
     email: "me@naver.com",
     avatar: "/chat/img/thumbnail_example.png",
+    gender: "Female",
     album: [
         {
+            id:0,
             url: "/chat/img/image_example.jpg",
             date: 1595174296810
         }, {
+            id:40,
             url: "/img/no-image.png",
             date: 1595174296820
         }, {
+            id:30,
             url: "/chat/img/thumbnail_example.png",
             date: 1595174296842
         }, {
+            id:12,
             url: "/img/logo/logo_endspring.png",
             date: 1595174296845
         }, {
+            id:87,
             url: "/chat/img/thumbnail_example.png",
             date: 1595174296852
         }, {
+            id:13,
             url: "/chat/img/thumbnail_example.png",
             date: 1595174296854
         }, {
+            id:9,
             url: "/chat/img/image_example.jpg",
             date: 1595174296855
         }, {
+            id:88,
             url: "/chat/img/image_example.jpg",
             date: 1595174296860
         }, {
+            id:33,
             url: "/chat/img/thumbnail_example.png",
             date: 159517429688
         }],
@@ -42,26 +52,35 @@ var tempUserModel = {
     follows: [{
             id: 1,
             avatar: 'choidanbi.jpeg',
-            nickname: 'Jieun'
+            nickname: 'Jieun',
+            state:"off"
         },
         {
             id: 2,
             avatar: 'choidanbi.jpeg',
-            nickname: 'danbi'
+            nickname: 'danbi',
+            state:"on"
         },
         {
             id: 3,
             avatar: 'choidanbi.jpeg',
-            nickname: '치민'
+            nickname: '치민',
+            state:"off"
         },
         {
             id: 4,
             avatar: 'choidanbi.jpeg',
-            nickname: '경호'
+            nickname: '경호',
+            state:"on"
+
+
         }]
 }
+
+
 app.factory('UserService', function() {
     return {
         user : tempUserModel
+
     };
 });
--- src/main/resources/static/mypage/js/mypage.js
+++ src/main/resources/static/mypage/js/mypage.js
@@ -1,45 +1,26 @@
 'use strict';
-app.controller('mypageCtrl',function ($scope) {
-    $scope.follows = [
-        {
-            src: 'choidanbi.jpeg',
-            name: 'Jieun'
-        },
-        {
-            src: 'choidanbi.jpeg',
-            name: 'danbi'
-        },
-        {
-            src: 'choidanbi.jpeg',
-            name: '치민'
-        },
-        {
-            src: 'choidanbi.jpeg',
-            name: '경호'
-        }
-    ]
 
+var nav = document.querySelector('.navLi');
 
+app.controller('mypageCtrl',function ($scope, $location,UserService) {
+    $scope.person = UserService.user;
+
+    $scope.selected = $location.absUrl().split("http://localhost:8080/")[1];
+
+    // 팔로워 삭제
     $scope.followerRemove = function (follow) {
-        var idx = $scope.follows.findIndex(function (item) {
-            return item.name == follow.name;
+        var idx = $scope.person.follows.findIndex(function (item) {
+            return item.id == follow.id;
         });
 
         if (idx > -1) {
-            $scope.follows.splice(idx, 1);
+            $scope.person.follows.splice(idx, 1);
         }
     }
 
 
-    $scope.person =
-        {
-            firstName: 'choi',
-            lastName: 'jieun',
-            nickName: 'danbi',
-            password: '1234',
-            src:'profile/choidanbi.jpeg'
 
-        }
+    // 정보 수정
     $scope.showHide = false;
     $scope.editText = "수정";
     $scope.editToggle = function () {
@@ -53,112 +34,35 @@ app.controller('mypageCtrl',function ($scope) {
         }
     }
 
-    $scope.images=[
-        {
-            src: 'choidanbi.jpeg',
-            id:1
-
-        },
-        {
-            src: 'choidanbi.jpeg',
-            id:2
-        },
-        {
-            src: 'choidanbi.jpeg',
-            id:3
-        },
-        {
-            src: 'choidanbi.jpeg',
-            id:4
-        }
-    ]
-
-
-    $scope.imgRemove = function (img) {
-        var idx = $scope.images.findIndex(function (item) {
-            return item.id == img.id;
-        })
-
-        if(idx>-1){
-            $scope.images.splice(idx,1)
-        }
-    };
-
+    // 프로필 수정
     $scope.changeProfileImg = function($event,element) {
 
         var reader = new FileReader();
-        alert("fd");
         reader.readAsDataURL(element.file);
 
         reader.onload = function (e) {
-            alert("ㅎㄷ");
-            $scope.person.src = e.target.result;
+            $scope.person.avatar = e.target.result;
             $scope.$apply();
 
         }
 
     }
 
+
+    // 보관함 이미지 삭제
+    $scope.imgRemove = function (img) {
+        var idx = $scope.person.album.findIndex(function (item) {
+            return item.id == img.id;
+        })
+
+        if(idx>-1){
+            $scope.person.album.splice(idx,1)
+        }
+    };
+
+
+
+
 });
-// $scope.nav = function (name) {
-//     return {
-//                 templateUrl: 'personal'
-//             };
-//
-// }
 
-
-// $scope.test = function(name){
-//         alert(name);
-//     //     app.directive('myCustomUrlFunction', function () {
-//     //     return {
-//     //         templateUrl: function (element) {
-//     //             return name;
-//     //         }
-//     //     };
-//     // });
-//     // $http({
-//     //     method: 'GET',
-//     //     url: name
-//     // }).then(function successCallback(response) {
-//     //     $scope.myHTML = response.data;
-//     //
-//     // }, function errorCallback(response) {
-//     //
-//     // });
-// }
-
-
-
-
-
-
-    // $scope.nav= '';
-    // $scope.mypage =
-    //
-    // $scope.rightWrap = $scope.
-
-    // $scope.test = function () {
-    //     alert('song');
-    //
-    //     return {
-    //         templateUrl: 'personal'
-    //     };
-    // }
-
-
-// app.directive('mydirc', function() {
-//     return {
-//         restrict: 'E',
-//         replace: true,
-//         // template: '<div></div>',
-//         link: function($scope, element, attrs) {
-//             $scope.test= function(name) {
-//                 return {
-//                     templateUrl: name
-//                 }
-//             }
-//         }
-//     }
-// });
 
