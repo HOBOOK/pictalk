6d924b74be5ba7824bb87fe9f293de1c1b4913bc
송치민 <vzcxds@gmail.com>
2020-07-26 20:17:38 +0900

    Update Notification-list delete function

--- src/main/resources/static/management/js/management.js
+++ src/main/resources/static/management/js/management.js
@@ -1,105 +1,54 @@
-'use strict';
 
-app.controller("accountCtrl", function ($scope, $uibModal, $http, $document) {
-    // $scope.show="false";
-    $scope.accounts = [{
+
+app.controller("alarmCtrl", function ($scope) {
+
+    $scope.showHide = false;
+
+    $scope.alarms = [{
         no: "1",
-        email: "park@gmail.com",
-        name: "박경호",
-        nickname: "호북이",
-        gender: "남",
-        profile: "/management/img/profile.png",
-        ban: "",
-        authority: 0,
-        connect: "on",
-        joinDate: "2020-07-09 20:00",
-        reportCount: 1
+        pofile: "/img/logo/logo.png",
+        name: "송치민",
+        content: "120번 채팅방 삭제",
+        time: "1"
     },{
         no: "2",
-        email: "vzcxds@gamil.com",
-        name: "송치민",
-        nickname: "송치킨",
-        gender: "남",
-        profile: "/management/img/profile.png",
-        ban: "",
-        authority: 1,
-        connect: "off",
-        joinDate: "2020-07-09 20:00",
-        reportCount: 10
+        pofile: "/img/logo/logo.png",
+        name: "박경호",
+        content: "121번 채팅방 삭제",
+        time: "2"
     },{
         no: "3",
-        email: "choi@gmail.com",
+        pofile: "/img/logo/logo.png",
         name: "최지은",
-        nickname: "최단비",
-        gender: "남",
-        profile: "/profile/choidanbi.jpeg",
-        ban: "날짜",
-        authority: 1,
-        connect: "ban",
-        joinDate: "2020-07-09 20:00",
-        reportCount: 112
+        content: "122번 채팅방 삭제",
+        time: "3"
     },{
         no: "4",
-        email: "kim@gmail.com",
-        name: "송아리",
-        nickname: "송알",
-        gender: "여",
-        profile: "/management/img/profile.png",
-        ban: "날짜",
-        authority: 1,
-        connect: "delete",
-        joinDate: "2020-07-09 20:00",
-        reportCount: 112
+        pofile: "/img/logo/logo.png",
+        name: "송치민",
+        content: "123번 채팅방 삭제",
+        time: "4"
+    },{
+        no: "5",
+        pofile: "/img/logo/logo.png",
+        name: "박경호",
+        content: "1102번 회원 정지",
+        time: "5"
     }];
 
 
-    angular.element(document).ready( function () {
-        $('#account-table').dataTable();
-    });
+    //Back단에서 읽음 처리하기
+    $scope.alarm_Allclear = function() {
+        $scope.alarms.splice(0);
+    };
+    $scope.alarm_clear = function (alarm) {
+        var idx = $scope.alarms.findIndex(function (item) {
+            return item.no == alarm.no;
+        })
 
-
-
-    $scope.openModal = function(account_info) {
-
-        // console.log(account_info);
-
-        var modalInstance = $uibModal.open({
-            templateUrl: '/console/info',
-            controller: 'infoController',
-            size: 'lg',
-            resolve: {
-                account_info: function () {
-                    return account_info;
-                },
-            }
-        });
-        modalInstance.result.then(function (item) {
-
-        }, function () {
-
-        });
-    }
-
-
-
-    // $scope.click = function() {
-    //
-    //     console.log('dddd');
-    //     $('#account-modal').modal('show');
-    //
-    //     $http({
-    //         method: 'GET',
-    //         url: 'info'
-    //     }).then(function successCallback(response) {
-    //         $scope.myHTML = response.data;
-    //
-    //     }, function errorCallback(response) {
-    //
-    //     });
-    // }
+        if(idx>-1){
+            $scope.alarms.splice(idx,1)
+        }
+    };
 
 });
-
-// app.config(['$qProvider', function ($qProvider) {
-//     $qProvider.errorOnUnhandledRejections(false);
-// }]);
