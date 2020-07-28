2993fc9324b7f25e6f4c99488d62a380f4274926
박경호 <pkh879@gmail.com>
2020-07-28 14:35:36 +0900

    Add configuration list to chat module

--- src/main/resources/static/chat/configuration/js/configuration.js
+++ src/main/resources/static/chat/configuration/js/configuration.js
@@ -1,3 +1,10 @@
-app.controller('configController',function ($scope) {
-    
-})
+var alertPopup = document.querySelector('common-alert');
+
+app.controller('configController',function ($scope, $timeout) {
+    $scope.saveConfig = function () {
+        alertPopup.classList.add('on');
+        $timeout(function () {
+            alertPopup.classList.remove('on');
+        },3000);
+    }
+});
